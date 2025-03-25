a66825966d533c2439797692d37c1d12302c90db
it24102843 <it24102843@my.sliit.lk>
2025-03-25 14:26:03 +0530

    Update Registration.java

--- Student course registration system/src/main/java/com/studentregistration/model/Registration.java
+++ Student course registration system/src/main/java/com/studentregistration/model/Registration.java
@@ -1,37 +1,37 @@
 package com.studentregistration.model;
 
-    public class Registration {
-        private String fullName;
-        private String email;
-        private String password;
-        private String course;
+public class Registration {
+    private String fullName;
+    private String email;
+    private String password;
+    private String course;
 
-        // Constructor
-        public Registration(String fullName, String email, String password, String course) {
-            this.fullName = fullName;
-            this.email = email;
-            this.password = password;
-            this.course = course;
-        }
-
-        // Getters and Setters
-        public String getFullName() { return fullName; }
-        public void setFullName(String fullName) { this.fullName = fullName; }
-
-        public String getEmail() { return email; }
-        public void setEmail(String email) { this.email = email; }
-
-        public String getPassword() { return password; }
-        public void setPassword(String password) { this.password = password; }
-
-        public String getCourse() { return course; }
-        public void setCourse(String course) { this.course = course; }
-
-        @Override
-        public String toString() {
-            return fullName + "," + email + "," + password + "," + course;
-        }
+    // Constructor
+    public Registration(String fullName, String email, String password, String course) {
+        this.fullName = fullName;
+        this.email = email;
+        this.password = password;
+        this.course = course;
     }
+
+    // Getters and Setters
+    public String getFullName() { return fullName; }
+    public void setFullName(String fullName) { this.fullName = fullName; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+
+    public String getPassword() { return password; }
+    public void setPassword(String password) { this.password = password; }
+
+    public String getCourse() { return course; }
+    public void setCourse(String course) { this.course = course; }
+
+    @Override
+    public String toString() {
+        return fullName + "," + email + "," + password + "," + course;
+    }
+}
 
 
 
