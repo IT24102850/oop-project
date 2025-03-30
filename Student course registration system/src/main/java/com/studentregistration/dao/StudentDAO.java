93c1da51368cf8f894b68985c05dc42438cbba5b
it24102843 <it24102843@my.sliit.lk>
2025-03-30 22:34:02 +0530

    Update StudentDAO.java

--- Student course registration system/src/main/java/com/studentregistration/dao/StudentDAO.java
+++ Student course registration system/src/main/java/com/studentregistration/dao/StudentDAO.java
@@ -56,8 +56,7 @@ public class StudentDAO {
                 .findFirst()
                 .orElse(null);
     }
-
-    // Register new student
+    
     public boolean registerStudent(Student student) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(STUDENT_FILE, true))) {
             writer.write(student.getUsername() + ":" + student.getPassword() + ":"
